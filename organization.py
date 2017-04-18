08c034f76346ab4f95fd194298014d7dafccad4e
unknown <brucebigirwenkya@gmail.com>
2017-04-18 16:29:50 +0300

    initial repo

---
+++ organization.py
@@ -0,0 +1,31 @@
+import math
+
+class Person(object):
+    def __init__(self, name, age, gender):
+        self.name
+        self.age
+        self.gender
+
+    def communicate(self, message):
+        return message
+
+    def move(self, distance):
+        return distance
+
+class Employee(Person):
+    def __init__(self, title):
+        self.id = math.random()
+        self.title = title
+    
+    def work(self, assignment):
+        move()
+        communicate()
+
+class Employer(Employee):
+    def __init__():
+        self.subordinates = []
+
+    def hire():
+        new_sub = new Employee('Bruce Bigirwenkya', 23, 'male' 'cleaner')
+        self.subordinates.append(new_sub)
+
